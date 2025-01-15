64b03c63dbf53632b50cc1d45d55dbdcf06f3fa5
Alexander Neumann <aleneum@gmail.com>
2025-01-15 11:41:26 +0100

    fix: missed escape in script

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ function hive_cli() {
         )
     fi
     docker login ghcr.io
-    if [ -n "${CLI_VERSION}" ]; then
+    if [ -n "\${CLI_VERSION}" ]; then
         echo "Pulling hive-cli version 'CLI_VERSION' ..."
         docker pull ghcr.io/caretech-owl/hive-cli:\${CLI_VERSION}
     fi
