6c93444455223252f3b1b61da204c4ab0ddd232f
Alexander Neumann <aleneum@gmail.com>
2025-01-16 15:50:50 +0100

    feat: use token when set and create a temporary remote

--- hive_cli/repo.py
+++ hive_cli/repo.py
@@ -1,11 +1,12 @@
 import logging
+import os
 import subprocess
 from datetime import datetime
 from pathlib import Path
 
-from git import Repo
+from git import Remote, Repo
+from pydantic import SecretStr
 
-from hive_cli.config import Settings
 from hive_cli.data import HiveData, RepoState
 
 _LOGGER = logging.getLogger(__name__)
@@ -77,16 +78,20 @@ class RepoController:
             self.hive.repo_state = RepoState.NOT_FOUND
             return None
         branch_name = (
-            f"{self.hive.settings.hive_id[:3]}_{self.hive.settings.hive_id[-3:]}-{int(datetime.now().timestamp())}"
+            f"{self.hive.settings.hive_id[:3]}_{self.hive.settings.hive_id[-3:]}-"
+            f"{int(datetime.now().timestamp())}"
         )
         _LOGGER.debug("Committing changes to remote branch %s", branch_name)
-        origin = self.repo.remote("origin")
         self.repo.create_head(branch_name).checkout()
         self.repo.index.add("*")
         self.repo.index.commit(
             "Changes made by hive-cli on " + datetime.now().isoformat()
         )
-        origin.push(branch_name)
+        origin = self.repo.remote("origin")
+
+        with TokenizedRemote(origin, self.hive.settings.github_token) as tokenized:
+            _LOGGER.debug("Pushing changes to remote")
+            tokenized.push(branch_name, kill_after_timeout=2.0)
         self.update_state()
 
     def update_state(self) -> None:
@@ -97,11 +102,44 @@ class RepoController:
         _LOGGER.debug("Fetching origin from %s", self.hive.settings.hive_url)
         self.repo.remote("origin").fetch()
 
-        if datetime.fromtimestamp(
+        if self.repo.active_branch.name != "main":
+            self.hive.repo_state = RepoState.CHANGES_COMMITTED
+        elif datetime.fromtimestamp(
             self.repo.head.commit.committed_date
         ) < datetime.fromtimestamp(self.repo.remote().refs.main.commit.committed_date):
             self.hive.repo_state = RepoState.UPDATE_AVAILABLE
-        elif self.repo.is_dirty() or len(self.repo.untracked_files) > 0:
+        elif (
+            self.repo.is_dirty()
+            or len(self.repo.untracked_files) > 0
+        ):
             self.hive.repo_state = RepoState.CHANGED_LOCALLY
         else:
             self.hive.repo_state = RepoState.UP_TO_DATE
+
+
+class TokenizedRemote:
+
+    def __init__(self, remote: Remote, token: str) -> None:
+        self.remote: Remote = remote
+        self.tmp_remote: Remote | None = None
+        self.token: SecretStr | None = token
+
+    def __enter__(self) -> Remote:
+        if self.token is None:
+            _LOGGER.debug("Using existing remote without token")
+            url = self.remote.url
+        else:
+            _LOGGER.debug("Creating temporary remote with token")
+            url = self.remote.url.replace(
+                "https://", f"https://{self.token.get_secret_value()}@"
+            )
+            if "github.com" not in url:
+                msg = "Only GitHub remotes are supported"
+                raise AssertionError(msg)
+        self.tmp_remote = self.remote.repo.create_remote("tmp", url)
+        return self.tmp_remote
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:  # noqa: ANN001
+        _LOGGER.debug("Removing temporary remote")
+        if self.tmp_remote is not None:
+            self.tmp_remote.remove(self.remote.repo, self.tmp_remote.name)
